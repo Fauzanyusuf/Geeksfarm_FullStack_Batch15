11e68e34650f02afb4f5a0ef40ea2c351f25fe5e
Fauzanyusuf <44010289+Fauzanyusuf@users.noreply.github.com>
2025-08-14 09:22:30 +0700

    Memindahkan proses fungsi ensureDataFile() ke initDataFile() pada fileUtils.js

--- tugas/tugas-02/utils/fileUtils.js
+++ tugas/tugas-02/utils/fileUtils.js
@@ -4,21 +4,17 @@ const path = require("path");
 const DATA_FOLDER = path.join(__dirname, "../data");
 const DATA_FILE = path.join(DATA_FOLDER, "data.json");
 
-async function ensureDataFile(filePath) {
-  const dir = path.dirname(filePath);
+async function initDataFile() {
+  const dir = path.dirname(DATA_FILE);
 
   await fs.mkdir(dir, { recursive: true });
 
   try {
-    await fs.access(filePath);
+    await fs.access(DATA_FILE);
   } catch {
-    await fs.writeFile(filePath, JSON.stringify([], null, 2));
-    console.log(`\nMembuat file baru: ${filePath}\n`);
+    await fs.writeFile(DATA_FILE, JSON.stringify([], null, 2));
+    console.log(`\nMembuat file baru: ${DATA_FILE}\n`);
   }
-}
-
-async function initDataFile() {
-  await ensureDataFile(DATA_FILE);
 }
 
 async function readData() {
@@ -31,7 +27,6 @@ async function saveData(data) {
 }
 
 module.exports = {
-  ensureDataFile,
   initDataFile,
   readData,
   saveData,
